c1dfa016547cc004ce7e2845a2e517a7c861cf4d
derpfear <kurtdebelen431@gmail.com>
2024-06-05 09:00:03 +0800

    Homev1

---
+++ prod1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Breakfast</title>
+    <script src="https://kit.fontawesome.com/0c3ddd4ac2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/navbot.css">
+    <link rel="stylesheet" href="css/prod.css">
+</head>
+
+<body>
+
+    <div class="nav-cont">
+        <nav class="navbar">
+            <div class="navimg">
+                <a href="">
+                    <img src="images/logo.png" alt="cdmlogo">
+                    <h1>CDM RESERVATION SYSTEM</h1>
+                </a>
+            </div>
+            <div class="navlinks">
+                <a href="index.php" id="link1"><i class="fa-solid fa-house"></i></a>
+                <div class="ddown">
+                    <a href="stores.php" id="link2"><i class="fa-solid fa-store"></i></a>
+                    <div class="ddown-content">
+                        <a href="#link1" id="l1">Breakfast <i class="fa-solid fa-angles-right" style="float: right;"></i></a>
+                        <a href="#link2" id="l2">Lunch <i class="fa-solid fa-angles-right" style="float: right;"></i></a>
+                        <a href="#link3" id="l3">Snacks <i class="fa-solid fa-angles-right" style="float: right;"></i></a>
+                        <a href="#link4" id="l4">Drinks <i class="fa-solid fa-angles-right" style="float: right;"></i></a>
+                    </div>
+                </div>
+                <a href="cart.php" id="link3"><i class="fa-solid fa-cart-shopping"></i></a>
+                <a href="users.php" id="link4"><i class="fa-regular fa-user"></i></a>
+                <a href="menu.php" id="link5"><i class="fa-solid fa-bars"></i></a>
+            </div>
+        </nav>
+    </div>
+
+    <main class="main-content">
+    </main>
+
+    <footer>
+        <div class="details">
+            <div class="det" id="det1"></div>
+            <div class="det" id="det2">
+                <a href="">About Us</a>
+                <a href="">Contact Us</a>
+                <a href="">Terms and Conditions</a>
+                <a href="">Privacy Notice</a>
+            </div>
+            <div class="det" id="det3">
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+
+</html>
